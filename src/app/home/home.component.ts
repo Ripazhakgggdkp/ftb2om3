08b9d7dd1b21200acaab6d22b8582b432716e1dd
Ripazhakgggdkp <ripazhakgggdkp@protonmail.com>
2018-03-13 16:20:01 -0500

    Update id3 after file change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,10 @@ import { convertFileToBuffer, fetchFileAsBuffer } from 'id3-parser/lib/universal
 import universalParse from 'id3-parser/lib/universal';
 
 
-import { QuoteService } from './quote.service';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { AbstractControl } from '@angular/forms/src/model';
+import { IID3Tag } from 'id3-parser/lib/interface';
 
 @Component({
   selector: 'app-home',
@@ -14,24 +17,52 @@ import { QuoteService } from './quote.service';
 })
 export class HomeComponent implements OnInit {
 
+  mp3Form: FormGroup;
+
   quote: string;
   isLoading: boolean;
   mp3: File = null;
 
-  constructor(private quoteService: QuoteService) { }
+  private title: AbstractControl;
+  private unicodeTitle: AbstractControl;
+  private artist: AbstractControl;
+  private unicodeArtist: AbstractControl;
+  private creator: AbstractControl;
+  private source: AbstractControl;
+
+  constructor(private fb: FormBuilder) {
+    this.mp3Form = this.fb.group({
+      title: '',
+      unicodeTitle: '',
+      artist: '',
+      unicodeArtist: '',
+      creator: '',
+      source: '',
+    });
+
+    this.title = this.mp3Form.get('title');
+    this.unicodeTitle = this.mp3Form.get('unicodeTitle');
+    this.artist = this.mp3Form.get('artist');
+    this.unicodeArtist = this.mp3Form.get('unicodeArtist');
+    this.creator = this.mp3Form.get('creator');
+    this.source = this.mp3Form.get('source');
+
+  }
 
   ngOnInit() {
     this.isLoading = true;
-    this.quoteService.getRandomQuote({ category: 'dev' })
-      .pipe(finalize(() => { this.isLoading = false; }))
-      .subscribe((quote: string) => { this.quote = quote; });
   }
 
   onMp3Change(files: FileList) {
-    console.log("OAIFJOAIJ");
     this.mp3 = files.item(0);
     convertFileToBuffer(this.mp3).then(parse).then(tag => {
-      console.log(tag);
+      const id3 = <IID3Tag>tag;
+      this.title.setValue(id3.title);
+      this.unicodeTitle.setValue(id3.title);
+      this.unicodeTitle.setValue(id3.title);
+      this.artist.setValue(id3.artist);
+      this.unicodeArtist.setValue(id3.artist);
     });
+
   }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,6 +7,7 @@ import { SharedModule } from '@app/shared';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { QuoteService } from './quote.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { QuoteService } from './quote.service';
     TranslateModule,
     CoreModule,
     SharedModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     HomeComponent
